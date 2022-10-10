8b582e46aa4d43540ace4a700d2b4f690946c984
Anupa Thudugala <95066941+AThudugala@users.noreply.github.com>
2022-10-10 14:57:49 +0530

    Add files via upload

---
+++ src/test/StartUp.java
@@ -0,0 +1,17 @@
+package test;
+
+import gui.Menu;
+
+/**
+ *
+ * @author Anupa Amawarni
+ */
+public class StartUp {
+    
+    public static void main(String[] args) {
+        Menu home = new Menu();
+        home.setTitle("Tic Tac Toe Ulimate ~ Home");
+        home.setVisible(true);
+    }
+    
+}
